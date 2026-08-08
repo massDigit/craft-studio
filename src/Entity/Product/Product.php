c00e3989f0e24d2b4cb86fef1cd7eb0b808d3e60
Samuel <samuel.zentou@my-digital-school.org>
2026-08-08 14:19:06 +0200

    feat: add ProductAudio domain model and Sylius resource mapping

---
+++ src/Entity/Product/Product.php
@@ -0,0 +1,83 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity\Product;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Sylius\Component\Core\Model\Product as BaseProduct;
+use Sylius\Component\Product\Model\ProductTranslationInterface;
+use Sylius\MolliePlugin\Entity\ProductInterface;
+use Sylius\MolliePlugin\Entity\ProductTrait;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'sylius_product')]
+class Product extends BaseProduct implements ProductInterface
+{
+    use ProductTrait;
+
+    /**
+     * @var Collection<int, ProductAudio>
+     */
+    #[ORM\OneToMany(mappedBy: 'product', targetEntity: ProductAudio::class, cascade: ['all'], orphanRemoval: true)]
+    private Collection $audios;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->audios = new ArrayCollection();
+    }
+
+    protected function createTranslation(): ProductTranslationInterface
+    {
+        return new ProductTranslation();
+    }
+
+    /**
+     * @return Collection<int, ProductAudio>
+     */
+    public function getAudios(): Collection
+    {
+        return $this->audios;
+    }
+
+    public function hasAudios(): bool
+    {
+        return !$this->audios->isEmpty();
+    }
+
+    public function addAudio(ProductAudio $audio): self
+    {
+        if (!$this->audios->contains($audio)) {
+            $this->audios->add($audio);
+            $audio->setProduct($this);
+        }
+
+        return $this;
+    }
+
+    public function removeAudio(ProductAudio $audio): self
+    {
+        if ($this->audios->removeElement($audio)) {
+            if ($audio->getProduct() === $this) {
+                $audio->setProduct(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function getPrimaryAudio(): ?ProductAudio
+    {
+        foreach ($this->audios as $audio) {
+            if ($audio->isPrimary()) {
+                return $audio;
+            }
+        }
+
+        return $this->audios->first() ?: null;
+    }
+}
---
+++ src/Entity/Product/ProductAudio.php
@@ -0,0 +1,150 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity\Product;
+
+use Doctrine\ORM\Mapping as ORM;
+use Sylius\Component\Resource\Model\ResourceInterface;
+use Symfony\Component\HttpFoundation\File\File;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'ztc_product_audio')]
+class ProductAudio implements ResourceInterface
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(targetEntity: Product::class, inversedBy: 'audios')]
+    #[ORM\JoinColumn(name: 'product_id', referencedColumnName: 'id', onDelete: 'CASCADE')]
+    private ?Product $product = null;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private ?string $path = null;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private ?string $originalName = null;
+
+    #[ORM\Column(type: 'string', length: 100, nullable: true)]
+    private ?string $mimeType = null;
+
+    #[ORM\Column(type: 'integer', nullable: true)]
+    private ?int $size = null;
+
+    #[ORM\Column(type: 'boolean', options: ['default' => true])]
+    private bool $isPrimary = true;
+
+    #[ORM\Column(type: 'datetime_immutable')]
+    private \DateTimeImmutable $createdAt;
+
+    /**
+     * File property for form handling & uploads (not persisted directly in DB column)
+     */
+    private ?File $file = null;
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTimeImmutable();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getProduct(): ?Product
+    {
+        return $this->product;
+    }
+
+    public function setProduct(?Product $product): self
+    {
+        $this->product = $product;
+
+        return $this;
+    }
+
+    public function getPath(): ?string
+    {
+        return $this->path;
+    }
+
+    public function setPath(?string $path): self
+    {
+        $this->path = $path;
+
+        return $this;
+    }
+
+    public function getOriginalName(): ?string
+    {
+        return $this->originalName;
+    }
+
+    public function setOriginalName(?string $originalName): self
+    {
+        $this->originalName = $originalName;
+
+        return $this;
+    }
+
+    public function getMimeType(): ?string
+    {
+        return $this->mimeType;
+    }
+
+    public function setMimeType(?string $mimeType): self
+    {
+        $this->mimeType = $mimeType;
+
+        return $this;
+    }
+
+    public function getSize(): ?int
+    {
+        return $this->size;
+    }
+
+    public function setSize(?int $size): self
+    {
+        $this->size = $size;
+
+        return $this;
+    }
+
+    public function isPrimary(): bool
+    {
+        return $this->isPrimary;
+    }
+
+    public function setIsPrimary(bool $isPrimary): self
+    {
+        $this->isPrimary = $isPrimary;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): \DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function getFile(): ?File
+    {
+        return $this->file;
+    }
+
+    public function setFile(?File $file): self
+    {
+        $this->file = $file;
+
+        return $this;
+    }
+
+    public function hasFile(): bool
+    {
+        return null !== $this->file;
+    }
+}
