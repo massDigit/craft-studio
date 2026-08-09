ad31b4905c71439b6af30be28e0ccf845773ea1a
Samuel <samuel.zentou@my-digital-school.org>
2026-08-09 16:36:16 +0200

    fix: synchronize ZtcCatalogFixture with InitZtcCatalogCommand product channel pricing and enabled state

--- src/Fixture/ZtcCatalogFixture.php
+++ src/Fixture/ZtcCatalogFixture.php
@@ -205,6 +205,7 @@ class ZtcCatalogFixture extends AbstractFixture
             /** @var Product $product */
             $product = $this->productFactory->createNew();
             $product->setCode($code);
+            $product->setEnabled(true);
             $product->setMainTaxon($taxon);
 
             foreach ($translations as $locale => $data) {
@@ -238,6 +239,13 @@ class ZtcCatalogFixture extends AbstractFixture
 
             $this->entityManager->persist($product);
         } else {
+            $product->setEnabled(true);
+            $product->setMainTaxon($taxon);
+
+            if ($channel && !$product->hasChannel($channel)) {
+                $product->addChannel($channel);
+            }
+
             foreach ($translations as $locale => $data) {
                 $this->addProductTranslation($product, $locale, $data['name'], $data['description']);
             }
@@ -255,18 +263,21 @@ class ZtcCatalogFixture extends AbstractFixture
             $variant = $this->productVariantFactory->createNew();
             $variant->setCode($code . '-default');
             $variant->setProduct($product);
+            $product->addVariant($variant);
+            $this->entityManager->persist($variant);
+        }
 
-            if ($channel) {
+        /** @var ProductVariantInterface $variant */
+        foreach ($product->getVariants() as $variant) {
+            if ($channel && !$variant->hasChannelPricingForChannel($channel)) {
                 /** @var ChannelPricingInterface $channelPricing */
                 $channelPricing = $this->channelPricingFactory->createNew();
                 $channelPricing->setChannelCode($channel->getCode());
                 $channelPricing->setPrice($priceInCents);
                 $channelPricing->setProductVariant($variant);
                 $variant->addChannelPricing($channelPricing);
+                $this->entityManager->persist($channelPricing);
             }
-
-            $product->addVariant($variant);
-            $this->entityManager->persist($variant);
         }
     }
 
